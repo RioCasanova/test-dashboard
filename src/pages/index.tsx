06423be9b415655121f7910bab15cf4b2375d1d6
Rio Casanova <riocasanova@gmail.com>
2024-04-26 13:06:49 -0600

    authentication: added logout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { signIn, useSession, signOut } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Welcome to the test dashboard
-          {/* <code className="font-mono font-bold">src/pages/index.tsx</code> */}
+          {session && session.user ? (
+            <span>
+              Welcome,{" "}
+              <code className="font-mono font-bold">{session.user.name}</code>
+            </span>
+          ) : (
+            <span>Welcome, please log in to get started</span>
+          )}
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -45,22 +53,37 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="/login"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Test Login{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Click here to test login functionality.
-          </p>
-        </a>
+        {session ? (
+          <button
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            onClick={() => signOut()}
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Sign Out
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Click here to sign out.
+            </p>
+          </button>
+        ) : (
+          <button
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            onClick={() => signIn("google")}
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Test Login
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Click here to test login functionality.
+            </p>
+          </button>
+        )}
       </div>
     </main>
   );
