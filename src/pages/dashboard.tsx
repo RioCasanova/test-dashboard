ad912423210a6119ac3e7d0e2f9367c26015fcc7
Rio Casanova <riocasanova@gmail.com>
2024-04-26 22:16:55 -0600

    Layout: made dashboard layout come out from under the header

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -8,13 +8,17 @@ const DashboardPage: React.FC = () => {
     <div>
       <Header imgLink="/dashboard" showLogout={true} />
       <Layout>
-        <div>
-          {/* Your user dashboard content here */}
-          <h1>Welcome to Your Dashboard</h1>
-          <p>
-            This is where you will see your fake data, graphs, metrics, etc.
-          </p>
-        </div>
+        <main className="relative py-48 max-md:px-4">
+          <div className="w-full max-w-[659px]">
+            <div>
+              {/* Your user dashboard content here */}
+              <h1>Welcome to Your Dashboard</h1>
+              <p>
+                This is where you will see your fake data, graphs, metrics, etc.
+              </p>
+            </div>
+          </div>
+        </main>
       </Layout>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
 
   return (
     <div className="background-image">
-      <Header showLogout={false} />
+      <Header imgLink="/" showLogout={false} />
       <Layout>
         <main className="relative py-48 max-md:px-4">
           <div className="w-full max-w-[659px]">
